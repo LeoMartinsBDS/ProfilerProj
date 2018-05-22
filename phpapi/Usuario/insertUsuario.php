3a69bba5b739406f21ff9b97f8422916ed5b0d85
LeoMartinsBDS <34400625+LeoMartinsBDS@users.noreply.github.com>
2018-05-22 00:27:05 -0300

    adicionado para pegar o ID do usuario quando ele se cadastrar e realizado ajustes no JSON de controle_usuario

--- phpapi/Usuario/insertUsuario.php
+++ phpapi/Usuario/insertUsuario.php
@@ -16,13 +16,30 @@ function createUsuario()
     $nome = $_POST["NOME"];
     $reputacao = $_POST["REPUTACAO"];
     $senha = $_POST["SENHA"];
+
+    $senha_md5 = md5($senha);
 	
-	$query = " INSERT INTO USUARIO(data_nascimento,email,foto,nome,reputacao,senha) VALUES ('$data_nascimento','$email','$foto','$nome','$reputacao','$senha');";
+	$query = " INSERT INTO USUARIO(data_nascimento,email,foto,nome,reputacao,senha) VALUES ('$data_nascimento','$email','$foto','$nome','$reputacao','$senha_md5');";
 
     $inserted = mysqli_query($connect, $query);
 
     if($inserted == 1){
-        $json['success'] = "Usuario inserido com sucesso!";
+
+        $querySelect = "SELECT LAST_INSERT_ID();";
+
+        $result = mysqli_query($connect, $querySelect);
+        $number_of_rows = mysqli_num_rows($result);
+
+        $temp_array  = array();
+
+        if($number_of_rows > 0) {
+            while ($row = mysqli_fetch_assoc($result)) {
+                $temp_array[] = $row;
+            }
+        }
+
+        header('Content-Type: application/json');
+        echo json_encode(array("codUsuario"=>$temp_array));
     }
     else{
         $json['error'] = "Houve um erro ao salvar usuario!";
